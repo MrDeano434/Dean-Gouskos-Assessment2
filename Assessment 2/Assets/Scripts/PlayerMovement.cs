761b9980788fd199cd15d6e7f102b3d7caefdc68
MrDeano434 <41457082+MrDeano434@users.noreply.github.com>
2018-08-16 13:06:07 +1000

    ROTATED MOVEMENT IS WORKING

--- Assessment 2/Assets/Scripts/PlayerMovement.cs
+++ Assessment 2/Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,7 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 movement;
     Rigidbody playerRigidbody;
-    
+
 
     void Start()
     {
@@ -22,11 +22,30 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-
-        Move(h, v);
-
+        if (rot.direction == Direction.Direction1)
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            Move(h, v);
+        }
+        else if (rot.direction == Direction.Direction2)
+        {
+            float h2 = Input.GetAxisRaw("Horizontal");
+            float v2 = Input.GetAxisRaw("Vertical");
+            MoveTwo(h2, v2);
+        }
+        else if (rot.direction == Direction.Direction3)
+        {
+            float h3 = Input.GetAxisRaw("Horizontal");
+            float v3 = Input.GetAxisRaw("Vertical");
+            MoveThree(h3, v3);
+        }
+        else if (rot.direction == Direction.Direction4)
+        {
+            float h4 = Input.GetAxisRaw("Horizontal");
+            float v4 = Input.GetAxisRaw("Vertical");
+            MoveFour(h4,v4);
+        }
 
         if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
@@ -34,47 +53,91 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+
+
     private bool IsGrounded()
     {
         return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
-        
+
     }
 
-    void Move (float h, float v)
+
+
+    void Move(float h, float v)
     {
-        movement.Set (h, 0f, v);
+        movement.Set(h, 0f, v);
 
         movement = movement.normalized * speed * Time.deltaTime;
 
         playerRigidbody.MovePosition(transform.position + movement);
     }
 
-   
 
 
-
-
-    void Update()
+    void MoveTwo(float h2, float v2)
     {
+        movement.Set(-v2, 0f, -h2);
 
-        //Movement();
-        /* void Movement()
-         {
-             if (Input.GetKeyDown(KeyCode.D))
-                 transform.Translate(1, 0, 0);
-         }
-         */    
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
     }
+
+
+
+    void MoveThree(float h3, float v3)
+    {
+        movement.Set(-h3, 0f, -v3);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+
+
+    void MoveFour(float h4, float v4)
+    {
+        movement.Set(v4, 0f, h4);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //void Update()
+    //{
+
+    //Movement();
+    /* void Movement()
+     {
+         if (Input.GetKeyDown(KeyCode.D))
+             transform.Translate(1, 0, 0);
+     }
+     */
+    //}
 
     /*void CheckRotation()
     {
         if (rot.direction == Direction.Direction1)
             
     }*/
-}
-
-
-
 
 
 /*  Need to reference camera direction script (4 directions)
